6d6901b9f7e16e7319cbbda0171432d5e7f66376
Colin Lord <colinlord@colinlord.com>
2017-10-08 15:46:11 -0400

    Update service worker

--- assets/js/sw.js
+++ assets/js/sw.js
@@ -1,6 +1,7 @@
-var CACHE_NAME = 'site-cache';
+var CACHE_NAME = 'my-site-cache-v1';
 var urlsToCache = [
   '/',
+  '/assets/json/manifest.json',
   '/assets/css/style.css',
   '/assets/js/main-header.js',
   '/assets/js/main-footer.js'
@@ -10,43 +11,9 @@ self.addEventListener('install', function(event) {
   // Perform install steps
   event.waitUntil(
     caches.open(CACHE_NAME)
-    .then(function(cache) {
-      console.log('Opened cache');
-      return cache.addAll(urlsToCache);
-    })
-  );
-});
-
-self.addEventListener('fetch', function(event) {
-  event.respondWith(
-    caches.match(event.request)
-    .then(function(response) {
-      // Cache hit - return response
-      if (response) {
-        return response;
-      }
-
-      // Clone the request
-      var fetchRequest = event.request.clone();
-
-      return fetch(fetchRequest).then(
-        function(response) {
-          // Check if we received a valid response
-          if (!response || response.status !== 200 || response.type !== 'basic') {
-            return response;
-          }
-
-          // Clone the response
-          var responseToCache = response.clone();
-
-          caches.open(CACHE_NAME)
-            .then(function(cache) {
-              cache.put(event.request, responseToCache);
-            });
-
-          return response;
-        }
-      );
-    })
+      .then(function(cache) {
+        console.log('Opened cache');
+        return cache.addAll(urlsToCache);
+      })
   );
 });
