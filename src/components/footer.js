f21abff711b4686f7efce84d329d9eeb04a27b06
Colin Lord <colin@kt4atl.com>
2019-11-11 10:45:52 -0600

    Create a footer component

---
+++ src/components/footer.js
@@ -0,0 +1,11 @@
+import { Link } from "gatsby"
+import React from "react"
+
+const Footer = () => (
+  <footer>
+    <p>The footer goes here.</p>
+    <p>This is a <Link to="/">to the homepage</Link>.</p>
+  </footer>
+)
+
+export default Footer
